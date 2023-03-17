2ac72eee1e1d73f9ea193cf370cb3495f16d4982
Carlos Estela <carlosestelablanco@gmail.com>
2023-03-17 20:40:39 +0100

    jwt.js updated

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -14,7 +14,7 @@ function authenticateToken(req, res, next) {
   if (token == null) return res.sendStatus(401);
 
   jwt.verify(token, process.env.JWT_SECRET, (err, data) => {
-    console.error(err);
+    console.log(err);
 
     if (err) return res.sendStatus(403);
 
