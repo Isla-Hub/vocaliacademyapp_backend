8c17d9b4f185d0fb5e0c7af932af112babaa392b
Meira81 <guadalupe.alonso.meira@gmail.com>
2023-07-23 21:17:46 +0200

    remove register endpoint and related test

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,6 +6,6 @@ import { createUser } from "../controllers/user.controller.js";
 const router = express.Router();
 
 router.route("/login").post(login);
-router.route("/register").post(createUser);
+
 
 export default router;
