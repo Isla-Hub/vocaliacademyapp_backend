1defea7c0c288187da175d27e5bb101eebb4dcbf
Guadi <guadalupe.alonso.meira@gmail.com>
2023-10-07 11:53:29 +0200

    change in cloud_name

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import {v2 as cloudinary} from 'cloudinary';
           
 cloudinary.config({
-  cloud_name: "dgtuznrav",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true,
